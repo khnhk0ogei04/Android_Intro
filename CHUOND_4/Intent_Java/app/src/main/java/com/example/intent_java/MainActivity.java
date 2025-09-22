a667dc4b1960cd981b8042225b668f07e9e28f25
khnhk0ogei04 <khanh8a04@gmail.com>
2025-09-22 23:20:27 +0700

    c4: intent_java

---
+++ CHUOND_4/Intent_Java/app/src/main/java/com/example/intent_java/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.intent_java;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.intent_java.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(mBinding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mBinding.btnLaunchActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SampleActivity.class);
+            startActivity(intent);
+        });
+    }
+}
