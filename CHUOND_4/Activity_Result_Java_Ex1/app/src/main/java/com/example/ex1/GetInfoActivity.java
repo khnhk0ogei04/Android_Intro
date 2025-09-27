0adf87a7cac6459ba75c1e38b952b2e557b8eb1d
khnhk0ogei04 <khanh8a04@gmail.com>
2025-09-27 10:17:33 +0700

    c4: startActivityForResult_ex1

--- CHUOND_4/Activity_Result_Java_Ex1/app/src/main/java/com/example/ex1/GetInfoActivity.java
+++ CHUOND_4/Activity_Result_Java_Ex1/app/src/main/java/com/example/ex1/GetInfoActivity.java
@@ -1,5 +1,6 @@
 package com.example.ex1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.ex1.databinding.ActivityGetInfoBinding;
+
+import java.util.Objects;
+
 public class GetInfoActivity extends AppCompatActivity {
+    private ActivityGetInfoBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_get_info);
-
+        mBinding = ActivityGetInfoBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        setupViews();
     }
+
+    private void setupViews(){
+        mBinding.btnSubmit.setOnClickListener(v -> {
+            String fullName = Objects.requireNonNull(mBinding.edtFullName.getText()).toString();
+            String gpaStr = Objects.requireNonNull(mBinding.edtGpa.getText()).toString();
+            float gpa = Float.parseFloat(gpaStr);
+            Intent intent = new Intent();
+            intent.putExtra(MainActivity.EXTRA_FULL_NAME, fullName);
+            intent.putExtra(MainActivity.EXTRA_GPA, gpa);
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+    }
+
 }
--- CHUOND_4/Activity_Result_Java_Ex1/app/src/main/java/com/example/ex1/MainActivity.java
+++ CHUOND_4/Activity_Result_Java_Ex1/app/src/main/java/com/example/ex1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ex1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,7 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ex1.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private static final int REQUEST_CODE = 100;
     public static final String EXTRA_FULL_NAME = "full_name";
     public static final String EXTRA_GPA = "gpa";
@@ -21,11 +24,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        mBinding.tvResult.setVisibility(View.GONE);
+        setupViews();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null && requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            String fullName = data.getStringExtra(EXTRA_FULL_NAME);
+            float gpa = data.getFloatExtra(EXTRA_GPA, 0.0F);
+
+            String result = getString(R.string.text_result, fullName, gpa);
+            mBinding.tvResult.setVisibility(View.VISIBLE);
+            mBinding.tvResult.setText(result);
+
+        }
+    }
+
+    private void setupViews(){
+        mBinding.btnStartActivity.setOnClickListener(v -> {
+            Intent childActivityIntent = new Intent(this, GetInfoActivity.class);
+            startActivityForResult(childActivityIntent, REQUEST_CODE);
         });
     }
+
 }
